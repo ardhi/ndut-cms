7f55f9b027a87b6053f7e3f0c432cee9e6977108
Ardhi Lukianto <ardhi@lukianto.com>
2022-11-23 09:42:07 +0700

    Updates

--- ndutRoute/route/media/@model/@id/@column/@file/get.js
+++ ndutRoute/route/media/@model/@id/@column/@file/get.js
@@ -3,11 +3,13 @@ module.exports = {
     const { mime } = this.ndut.helper
     if (!['cms-blog'].includes(request.params.model)) this.Boom.notFound('resourceNotFound')
     request.silentOnError = true // TODO: put on config and attach on preHandler
+    const filter = this.ndutRoute.helper.buildFilter(request)
     const { file, stream } = await this.ndutApi.helper.handleAttachment({
       model: request.params.model,
       id: request.params.id,
       column: request.params.column,
-      file: request.params.file
+      file: request.params.file,
+      filter
     })
     if (request.query.inline) reply.header('Content-Type', mime.getType(file))
     reply.send(stream)
